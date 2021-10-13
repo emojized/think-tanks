0b737b8404d9426dc7f59b1b6bb9e412441144ae
emojized <emojized@emojized.com>
2021-10-13 16:23:11 +0200

    added map files

---
+++ maps/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrainTiles_default" tilewidth="63" tileheight="64" spacing="1" tilecount="30" columns="10">
+ <image source="../Tilesheet/terrainTiles_default.png" width="640" height="256"/>
+</tileset>
