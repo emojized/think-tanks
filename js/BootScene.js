e1ca574bdedc6a0a151e1afa476276d98dbafcd8
David <david.jordans@twofold.swiss>
2021-10-20 12:05:22 +0200

    added Tiled map

--- js/BootScene.js
+++ js/BootScene.js
@@ -5,12 +5,17 @@ class BootScene extends Phaser.Scene {
     preload() {
         this.load.image('sky', 'sky.png');
         this.load.image('tank', 'PNG/Default/tank_dark.png');
-        this.load.image('bullet', 'PNG/Default/bulletBlue1.png')
+        this.load.image('bullet', 'PNG/Default/bulletBlue1.png');
 
-        this.load.spritesheet('terrain',
+        this.load.image('tiles', 'Tilesheet/terrainTiles_default.png');
+		
+		this.load.tilemapTiledJSON('tilemap', 'maps/terrain.json');
+		
+		
+		/*this.load.spritesheet('terrain',
             'Tilesheet/terrainTiles_default.png',
             { frameWidth: 64, frameHeight: 64 }
-        );
+        ); */
     }
     create() {
 
